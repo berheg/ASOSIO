863805d4eb2f3d13a5c645eede69ead47acfe490
Girmay <ink423103@yahoo.com>
2020-05-08 08:40:54 +0200

    server port modified

--- src/server/app.js
+++ src/server/app.js
@@ -1,18 +1,9 @@
-//equire('dotenv').config();
-
 const express = require('express');
-//const path = require('path');
-//const morgan = require('morgan');
-//const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const HttpError = require('./api/lib/utils/http-error');
 
-//const buildPath = path.join(__dirname, '../../dist')
-
 const randomAPIresponseRouter = require('./api/routes/randomAPIresponse.router');
-
-//require('./config/db');
 
 const app = express();
 
@@ -37,21 +28,8 @@ app.use(
     parameterLimit: 50000,
   }),
 );
-//app.use(cookieParser());
+
 app.use(cors());
-
-//app.use(process.env.API_PATH, apiRouter);
-
-/*app.use((err, req, res) => {
-  if (err instanceof HttpError) {
-    res.status(err.httpStatus);
-    if (err.body) {
-      return res.json(err.body);
-    }
-    return res.send({ error: err.message });
-  }
-  res.sendStatus(500);
-});*/
 
 app.use('/api/', function(req, res) {
   res.status(404).send("Sorry can't find that!");
@@ -66,7 +44,7 @@ app.use((error, req, res, next) => {
    }
  });
 });
-const port= process.env.PORT || 3000;
+const port= process.env.PORT || 5000;
 app.listen(port, () =>{
   console.log(`Server listening on port ${port}`);
 });

a45e78326d5611007bfcd8d385112eb575cb64f5
Girmay <ink423103@yahoo.com>
2020-05-08 08:18:46 +0200

    server port modified

--- src/server/app.js
+++ src/server/app.js
@@ -66,7 +66,7 @@ app.use((error, req, res, next) => {
    }
  });
 });
-const port= process.env.PORT || 5000;
+const port= process.env.PORT || 3000;
 app.listen(port, () =>{
   console.log(`Server listening on port ${port}`);
 });

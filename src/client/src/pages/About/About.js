7f1c1ab4431f300d85b36ed0aefe92a3371df591
Girmay <ink423103@yahoo.com>
2020-05-09 15:12:24 +0200

    About modified

--- src/client/src/pages/About/About.js
+++ src/client/src/pages/About/About.js
@@ -4,9 +4,9 @@ function About() {
   return (
     <React.Fragment>
       <h1>About</h1>
-      <p>Sort the reactions generated from API and count the reactions with the same content or type.</p>
-      <p>The randomly generated array is fetched and converted to array of an object with keys type,  </p>
-      <p>content, names and numberOfNames. Finally, the numberOfNames, content and names are displayed as lists.</p>
+      <p>Sort reactions generated from API and count reactions with the same content or type. The randomly</p>
+      <p> generated array is fetched and converted to array of an object with keys type, content, names and </p>
+      <p> numberOfNames. Finally, the numberOfNames, content and names are displayed as lists.</p>
  
     </React.Fragment>
   )
